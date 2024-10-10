88f2bb06f712929b3dc86aa5b9c55a03e759fcc4
Johan Kladder <johankladder@gmail.com>
2024-10-10 13:21:42 +0200

    *

--- bin/printing/handler.py
+++ bin/printing/handler.py
@@ -32,6 +32,7 @@ class PrintHandler():
         path = pdf.stream.name
 
         for processor in self.processors:
+            print("TEST processing")
             processor.print_page(
                 print_payload=print_payload,
                 path=path
